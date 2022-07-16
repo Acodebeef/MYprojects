546d1637b67ccdf82f1c1c8763080d59223e7b3c
ender <109395556+Acodebeef@users.noreply.github.com>
2022-07-16 08:32:35 -0500

    Add files via upload

---
+++ gridusingtrtl.py
@@ -0,0 +1,39 @@
+import turtle as t
+t.speed(10.5)
+t.pensize(3)
+t.penup()
+t.forward(-300)
+t.left(90)
+t.forward(300)
+t.pendown()
+
+
+for i in range(4):
+    t.right(90)
+    t.forward(600)
+
+for i in range(8):
+    t.right(90)
+    t.forward(37.5)
+    t.right(90)
+    t.forward(600)
+    t.left(90)
+    t.forward(37.5)
+    t.left(90)
+    t.forward(600)
+t.right(90)
+for i in range(8):
+    
+    t.right(90)
+    t.forward(37.5)
+    t.right(90)
+    t.forward(600)
+    t.left(90)
+    t.forward(37.5)
+    t.left(90)
+    t.forward(600)
+
+
+    
+
+
