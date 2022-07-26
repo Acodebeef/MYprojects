218c319ba9eead04300cb267d2a63b5eeaa3aab3
ender <109395556+Acodebeef@users.noreply.github.com>
2022-07-26 17:24:44 -0500

    Add files via upload

---
+++ sun.py
@@ -0,0 +1,20 @@
+import turtle as t
+t.penup()
+t.back(120)
+t.left(90)
+t.pendown()
+t.speed(10)
+
+for i in range(15):
+    def face():
+        for i in range(4):
+            t.right(90)
+            t.forward(10 nb0)
+        
+    def back():
+        for i in range(1):
+            t.left(336)
+            t.forward(100)
+            face()
+            
+    back()
